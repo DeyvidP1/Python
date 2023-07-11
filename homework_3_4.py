70a58a06091b830ac1cacd5fae9cfb5fd8335904
DeyvidP1 <138444858+DeyvidP1@users.noreply.github.com>
2023-07-11 13:35:34 +0300

    Add files via upload

--- homework_3_4.py
+++ homework_3_4.py
@@ -14,23 +14,6 @@ def group(lst):
     result.append(temporary_result)
     return result
 
-def max_consectuive(items):
-    max_num = 1
-    current_num = 1
-
-    for item in range (1, len(items)):
-        if items[item] == items [item -1]:
-            current_num = current_num + 1
-
-        if current_num > max_num:
-            max_num = current_num
-        
-        else:
-            current_num = 1
-
-    return max_num
-
     pass
 
 print (group([1, 2, 1, 2, 3, 3]))
-print (max_consectuive([1, 2, 3, 3, 3, 3, 4, 3, 3]))
